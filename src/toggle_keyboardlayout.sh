6ea02ad2d5a703cfc70b25e8d81062382be9665f
Norman Köster <nkoester@techfak.uni-bielefeld.de>
2016-03-07 11:55:16 +0100

    Add compose option

--- src/toggle_keyboardlayout.sh
+++ src/toggle_keyboardlayout.sh
@@ -4,11 +4,11 @@ seven=`xmodmap -pke | grep "keycode  16" | awk '{print \$5}'`
 
 ## If this is the "us" layout
 if [ $seven == "ampersand" ]; then
-    setxkbmap de
+    setxkbmap -option caps:escape,compose:menu de
     notify-send "Keyboard Layout set to: German (de)" -t 2000 -u low -i /usr/share/icons/gnome/48x48/status/dialog-information.png
     xset led 3
 else
-    setxkbmap us
+    setxkbmap -option caps:escape,compose:menu us
     notify-send "Keyboard Layout set to: English (us)" -t 2000 -u low -i /usr/share/icons/gnome/48x48/status/dialog-information.png
     xset -led 3
 fi
