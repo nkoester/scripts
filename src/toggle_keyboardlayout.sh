36798f412e5c5ade53ce9f829acaf2f310906639
Norman Köster <nkoester@techfak.uni-bielefeld.de>
2019-09-02 11:54:12 +0200

    Modify keyboardtoggle

--- src/toggle_keyboardlayout.sh
+++ src/toggle_keyboardlayout.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-seven=`xmodmap -pke | grep "keycode  16" | awk '{print \$5}'` 
+seven=`xmodmap -pke | grep "keycode  16" | awk '{print \$5}'`
 
 ## If this is the "us" layout
 if [ $seven == "ampersand" ]; then
     setxkbmap -option caps:escape,compose:menu de
     notify-send "Keyboard Layout set to: German (de)" -t 2000 -u low -i /usr/share/icons/gnome/48x48/status/dialog-information.png
     xset led 3
+    xmodmap "$HOME/.Xmodmap-de"
 else
     setxkbmap -option caps:escape,compose:menu us
     notify-send "Keyboard Layout set to: English (us)" -t 2000 -u low -i /usr/share/icons/gnome/48x48/status/dialog-information.png
     xset -led 3
+    xmodmap "$HOME/.Xmodmap"
 fi
 
