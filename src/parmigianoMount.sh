9e742fa8618afafaa98c9d55f108720b1d5f37ba
Norman Köster <nkoester@techfak.uni-bielefeld.de>
2019-12-16 18:10:29 +0100

    Use array for mount table

--- src/parmigianoMount.sh
+++ src/parmigianoMount.sh
@@ -6,41 +6,72 @@ if [[ -z $1 ]]; then
     exit 1
 fi
 
+## declare an array variable
+# You can access them using echo "${arr[0]}", "${arr[1]}" also
+declare -a arr=("private"
+                "repository"
+                "games"
+                "shared"
+                "borgbackup"
+                "download"
+                "video"
+                )
 
-if [ "$1" == "m" ]; then
-    echo " --> Will mount folders ..."
-    udevil mount -t nfs parmigiano:/mnt/mainpool/private /media/parmigiano-private
-    udevil mount -t nfs parmigiano:/mnt/mainpool/repository /media/parmigiano-repository
-    udevil mount -t nfs parmigiano:/mnt/mainpool/games /media/parmigiano-games
-    udevil mount -t nfs parmigiano:/mnt/mainpool/shared /media/parmigiano-shared
-    udevil mount -t nfs parmigiano:/mnt/mainpool/borgbackup /media/parmigiano-borgbackup
-    udevil mount -t nfs parmigiano:/mnt/mainpool/download /media/parmigiano-download
+echo " --> Will mount folders ..."
 
-elif [ "$1" ==  "r" ]; then
-    echo " --> Will re-mount folders ..."
-    udevil mount -o remount parmigiano:/mnt/mainpool/private
-    udevil mount -o remount parmigiano:/mnt/mainpool/repository
-    udevil mount -o remount parmigiano:/mnt/mainpool/games
-    udevil mount -o remount parmigiano:/mnt/mainpool/shared
-    udevil mount -o remount parmigiano:/mnt/mainpool/borgbackup
-    udevil mount -o remount parmigiano:/mnt/mainpool/download
+for i in "${arr[@]}"
+do
+    if [ "$1" == "m" ]; then
+        echo " --> Mount ${i}"
+        udevil mount -t nfs parmigiano:/mnt/mainpool/${i} /mnt/parmigiano-${i}
+    elif [ "$1" ==  "r" ]; then
+        echo " --> Re-mount ${i}"
+        udevil mount -o remount parmigiano:/mnt/mainpool/${i}
+    elif [ "$1" ==  "u" ]; then
+        echo " --> Unmount ${i}"
+        udevil umount /mnt/parmigiano-${i}
+    else
+        echo "Error: Unknown goal '$1'"
+        exit 1
+    fi
+done
 
-    # udevil mount -o remount /media/nfs-parmigiano-mnt-mainpool-private
-    # udevil mount -o remount /media/nfs-parmigiano-mnt-mainpool-repository
-    # udevil mount -o remount /media/nfs-parmigiano-mnt-mainpool-games
 
-elif [ "$1" ==  "u" ]; then
-    echo " --> Will unmount folders ..."
-    udevil umount /media/parmigiano-private
-    udevil umount /media/parmigiano-repository
-    udevil umount /media/parmigiano-games
-    udevil umount /media/parmigiano-shared
-    udevil umount /media/parmigiano-borgbackup
-    udevil umount /media/parmigiano-download
-else
-    echo "Error: Unknown goal '$1'"
-    exit 1
-fi
+
+# if [ "$1" == "m" ]; then
+#     echo " --> Will mount folders ..."
+#     udevil mount -t nfs parmigiano:/mnt/mainpool/private /mnt/parmigiano-private
+#     udevil mount -t nfs parmigiano:/mnt/mainpool/repository /mnt/parmigiano-repository
+#     udevil mount -t nfs parmigiano:/mnt/mainpool/games /mnt/parmigiano-games
+#     udevil mount -t nfs parmigiano:/mnt/mainpool/shared /mnt/parmigiano-shared
+#     udevil mount -t nfs parmigiano:/mnt/mainpool/borgbackup /mnt/parmigiano-borgbackup
+#     udevil mount -t nfs parmigiano:/mnt/mainpool/download /mnt/parmigiano-download
+#
+# elif [ "$1" ==  "r" ]; then
+#     echo " --> Will re-mount folders ..."
+#     udevil mount -o remount parmigiano:/mnt/mainpool/private
+#     udevil mount -o remount parmigiano:/mnt/mainpool/repository
+#     udevil mount -o remount parmigiano:/mnt/mainpool/games
+#     udevil mount -o remount parmigiano:/mnt/mainpool/shared
+#     udevil mount -o remount parmigiano:/mnt/mainpool/borgbackup
+#     udevil mount -o remount parmigiano:/mnt/mainpool/download
+#
+#     # udevil mount -o remount /mnt/nfs-parmigiano-mnt-mainpool-private
+#     # udevil mount -o remount /mnt/nfs-parmigiano-mnt-mainpool-repository
+#     # udevil mount -o remount /mnt/nfs-parmigiano-mnt-mainpool-games
+#
+# elif [ "$1" ==  "u" ]; then
+#     echo " --> Will unmount folders ..."
+#     udevil umount /mnt/parmigiano-private
+#     udevil umount /mnt/parmigiano-repository
+#     udevil umount /mnt/parmigiano-games
+#     udevil umount /mnt/parmigiano-shared
+#     udevil umount /mnt/parmigiano-borgbackup
+#     udevil umount /mnt/parmigiano-download
+# else
+#     echo "Error: Unknown goal '$1'"
+#     exit 1
+# fi
 
 echo " --> All done."
 
